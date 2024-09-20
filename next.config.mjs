2cbdc2f90382855e5551b217ff1ff49277738ed6
Dylan <dylanmarin2018@gmail.com>
2024-09-20 15:57:47 -0600

    preparing for docker deployment

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
+  output: 'standalone',
+  reactStrictMode: true,
 };
 
 export default nextConfig;
